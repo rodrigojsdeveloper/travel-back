307a9fcfc9d5e2ba14be6ae3263491873ce4a84d
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-07-20 22:55:43 -0300

    feat: add route

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello')
+import fastify from "fastify"
+
+const app = fastify()
+
+app.get('/test', () => {
+  return 'Hello peoples!'
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server running!')
+})
