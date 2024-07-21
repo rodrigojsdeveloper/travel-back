52189d95c30be45d17df9f1dbd5b6b7c216cf133
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-07-21 02:30:48 -0300

    feat: add participant to generate confirmation email

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -15,10 +15,11 @@ export const createTrip = async (app: FastifyInstance) => {
         ends_at: z.coerce.date(),
         owner_name: z.string(),
         owner_email: z.string().email(),
+        emails_to_invite: z.array(z.string().email()),
       })
     }
   }, async (request) => {
-    const { destination, starts_at, ends_at, owner_name, owner_email } = request.body;
+    const { destination, starts_at, ends_at, owner_name, owner_email, emails_to_invite } = request.body;
 
     if(dayjs(starts_at).isBefore(new Date())) {
       throw new Error("Invalid trip start date.")
@@ -32,7 +33,22 @@ export const createTrip = async (app: FastifyInstance) => {
       data: {
         destination,
         starts_at,
-        ends_at
+        ends_at,
+        participants: {
+          createMany: {
+            data: [
+              {
+                name: owner_name,
+                email: owner_email,
+                is_confirmed: true,
+                is_owner: true,
+              },
+              ...emails_to_invite.map(email => {
+                return { email }
+              })
+            ]
+          }
+        }
       }
     })
 
@@ -48,7 +64,7 @@ export const createTrip = async (app: FastifyInstance) => {
         address: owner_email,
       },
       subject: "Test send e-mail",
-      html: `<p>Hello</p>`
+      html: `<p>Hello</p>`.trim()
     })
 
     console.log(nodemailer.getTestMessageUrl(message))
